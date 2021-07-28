909d6ee90468ab1001eed762cfb0d112ac0bb5f3
Justin Z <contactjustinzamora@gmail.com>
2021-07-27 23:49:31 -0700

    Changed browser start

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,9 @@ const Home = () => {
     return (
         <div>
             Home
+            <button className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+                Some button
+                </button>
         </div>
     )
 }
