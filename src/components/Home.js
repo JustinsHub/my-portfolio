72723e39e65ed70c6c106ea72c4c9d0c0d8da7b4
Justin Z <contactjustinzamora@gmail.com>
2021-07-29 11:09:11 -0700

    added UI to home buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import profilePic from  '../static/images/ProfilePic.png'
+import '../static/Home.css'
+import '../static/global.css'
 
 const Home = () => {
 
@@ -9,22 +11,25 @@ const Home = () => {
             <div className="grid grid-rows-1 py-24 px-5">
                 <div className="grid grid-cols-6 gap-4 mt-10 py-12"> 
                     <div className="col-start-2 col-span-2 py-12">
-                        <h1 className='text-4xl font-normal leading-normal mt-0 mb-2 flex justify-center'>Justin Zamora</h1>
-                        <h3 className="flex justify-center">Software Engineer from Los Angeles</h3>
+                        <h1 className='Home-name-font global-font font-extrabold text-5xl leading-normal mt-0 flex justify-center'>Justin Zamora</h1>
+                        <h3 className="Home-sub-font flex justify-center global-font text-lg font-medium">Software Engineer from Los Angeles</h3>
 
                     {/* Navigation buttons */}
                     <div className="m-1">
                         <div className="flex justify-center">
-                            <button className="bg-blueGray-500 text-black active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">About</button>
+                            <button className="Home-btn-bg Home-font-color font-bold uppercase text-xs py-2 rounded-full mr-1 mb-1 transition-all duration-200" type="button">Home</button>
                         </div>
                         <div className="flex justify-center">
-                            <button className="bg-lightBlue-100 text-black active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">Projects</button>
+                            <button className="Home-btn-bg Home-font-color font-bold uppercase text-xs py-2 rounded-full mr-1 mb-1 transition-all duration-200" type="button">About</button>
                         </div>
                         <div className="flex justify-center">
-                            <button className="bg-lightBlue-100 text-black active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">Skills</button>
+                            <button className="Home-btn-bg Home-font-color font-bold uppercase text-xs py-2 rounded-full  mr-1 mb-1 transition-all duration-200" type="button">Projects</button>
                         </div>
                         <div className="flex justify-center">
-                            <button className="bg-lightBlue-100 text-black active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">Contact</button>
+                            <button className="Home-btn-bg Home-font-color font-bold uppercase text-xs py-2 rounded-full  mr-1 mb-1 transition-all duration-200" type="button">Skills</button>
+                        </div>
+                        <div className="flex justify-center">
+                            <button className="Home-btn-bg Home-font-color font-bold uppercase text-xs py-2 rounded-full  mr-1 mb-1 transition-all duration-200" type="button">Contact</button>
                         </div>
                     </div>
                 </div>
